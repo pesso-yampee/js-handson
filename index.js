ffbb8aaf3cf69b06b124f873959d07d0a36932a0
Yamasaki Junpei <62529950+pesso-yampee@users.noreply.github.com>
2021-07-19 07:55:45 +0900

    done handson-12

--- index.js
+++ index.js
@@ -1,73 +1,105 @@
-const target = document.getElementById("target");
+const $target = document.getElementById("target");
 
-function addLoadingGif(mark) {
-  const loading = document.createElement("img");
+function createList(data, parent) {
+  const fragment = document.createDocumentFragment(); // DOMにまとめて追加するため
 
-  loading.setAttribute("id", "loading");
-  loading.src = "loading-circle.gif";
-  mark.prepend(loading);
+  data.data.forEach((item) => {
+    const $li = document.createElement("li");
+    const $a = document.createElement("a");
+    const $img = document.createElement("img");
+    const $p = document.createElement("p");
+
+    $a.textContent = item.text;
+    $a.href = item.to;
+    $img.src = item.img;
+    $img.alt = item.alt;
+    $p.className = "picture";
+
+    $p.appendChild($img);
+    $li.appendChild($a);
+    $li.appendChild($p);
+    fragment.appendChild($li);
+  });
+
+  parent.appendChild(fragment);
+}
+
+function addLoadingGif() {
+  const $loadingGif = document.createElement("img");
+  const $body = document.querySelector("body");
+
+  $loadingGif.src = "loading-circle.gif";
+  $loadingGif.alt = "loading-circle";
+  $loadingGif.id = "loading";
+  $body.appendChild($loadingGif);
 }
 
 function removeLoadingGif() {
-  const loading = document.getElementById("loading");
+  const $loadingGif = document.getElementById("loading");
 
-  loading.remove();
+  $loadingGif.remove();
 }
 
-function createList(lists) {
-  lists.forEach((list) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    const img = document.createElement("img");
+function cretateRequestButton() {
+  const $button = document.createElement("div");
+  const $body = document.querySelector("body");
 
-    a.href = list.to;
-    a.textContent = list.text;
-    img.src = list.img;
-    img.alt = list.alt;
-
-    target.appendChild(li).appendChild(a).appendChild(img);
-  });
+  $button.id = "request";
+  $button.className = "btn__request";
+  $button.textContent = "リクエスト";
+  $body.appendChild($button);
 }
+cretateRequestButton();
 
-async function fetchResource() {
+function removeRrequestButton() {
+  const $button = document.getElementById("request");
+
+  $button.remove();
+}
+// サーバーリクエスト
+async function request() {
   const res = await fetch(
     "https://jsondata.okiba.me/v1/json/xHouO210704011014"
   );
-  const json = await res.json();
-  const resource = json.data;
+  const json = res.json(); //レスポンスのjsonを解析
 
-  return resource;
-}
-
-// サーバーにリクエスト
-async function request() {
   return new Promise((resolve) => {
-    const data = fetchResource();
-
     setTimeout(() => {
-      resolve(data);
+      resolve(json);
     }, 3000);
   });
 }
 
+// サーバーから返ってきた情報を読み込む
 async function fetchData() {
   let res = null;
 
-  // 取得している間はローディング画像を表示
-  addLoadingGif(target);
   try {
+    addLoadingGif(); //サーバーリクエスト中は回す
     res = await request();
   } catch (error) {
-    console.log(error);
+    console.log(new Error(error + "が発生しました"));
   } finally {
-    removeLoadingGif(); //必ずresが返ってくるので閉じる
-    return res; //dataが取れなくても返す
+    removeLoadingGif();
+    return res; //中身があろうがなかろうが返す
   }
 }
 
-// データの初期化
+// 初期化
 async function init() {
-  const data = await fetchData();
-  createList(data);
+  const fetch = await fetchData();
+
+  createList(fetch, $target);
 }
-init();
+
+// リクエストを実行
+function executeRequest() {
+  const requestButton = document.getElementById("request");
+  requestButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    removeRrequestButton();
+    init();
+  });
+}
+
+executeRequest();
